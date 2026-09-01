08b267949128559d98cabf3e8784fe1c1ce02b33
Nyx1197 <1197457256@qq.com>
2026-09-01 16:52:32 +0800

    feat: support deployment under a nested directory
    - Change vite base to "./" for relative path loading
    - Allow the app to be deployed under any sub-path

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,8 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    // 资源使用相对路径以支持可以和其他网站共存或存放于二级目录
+    base: "./",
     plugins: [
         react(),
         // @vitejs/plugin-react 6 移除了 babel 选项，
